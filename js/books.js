5ea5680c20c6f9c9b46be5c18402af26d82d8f5f
Dadi <nafi.dadi@gmail.com>
2013-11-30 13:14:55 -0600

    modified css and js

--- js/books.js
+++ js/books.js
@@ -3,6 +3,10 @@ $('#colors li').click ( function() {
 	$('#content').val( $('#content').val() + colorValue + ';\n' );
 });
 
+$('#border_style li').click ( function() {
+        $('#content').val( $('#content').val() + $(this).attr('id') + ';\n' );
+});
+
 /* rgb2hex(rgb) function is borrowed from www.jquery4u.com */
 function rgb2hex(rgb){
  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
@@ -12,12 +16,54 @@ function rgb2hex(rgb){
   ("0" + parseInt(rgb[3],10).toString(16)).slice(-2);
 }
 
-$('#color_attr').change ( function() {
-	if($('#color_attr').val()==0) {
-		$('#colors').hide();
+$('#color_attr, #border_attr, #html_tags').change ( function() {
+	if($(this).val()==0) {
+		$('#colors, #border_style, #sections, #groupings, #embeddings, #tables, #forms, #hyperlinks').hide();
 	} else {
-		var attrValue = $('#color_attr option:selected').text() + ": ";
-		$('#content').val( $('#content').val() + attrValue );
-		$('#colors').show();
+		//var attrValue = $('option:selected', this).text() + ": ";
+		//$('#content').val( $('#content').val() + attrValue );
+		if( $(this).text() == $('#color_attr').text() ) {
+			var attrValue = $('option:selected', this).text() + ": ";
+                	$('#content').val( $('#content').val() + attrValue );
+			$('#colors').show();
+			$('#border_style, #sections, #groupings, #embeddings, #tables, #forms, #hyperlinks').hide();
+		} else if( $(this).text() == $('#border_attr').text() ) {
+			var attrValue = $('option:selected', this).text() + ": ";
+                	$('#content').val( $('#content').val() + attrValue );
+                        $('#border_style').show();
+			$('#colors, #sections, #groupings, #embeddings, #tables, #forms, #hyperlinks').hide();
+                } else if( $(this).text() == $('#html_tags').text() ) {
+			switch( $('option:selected', this).text() ) {
+			case 'Sections':
+                        	$('#sections').show();
+                        	$('#colors, #border_style, #groupings, #embeddings, #tables, #forms, #hyperlinks').hide();
+				break;
+			case 'Grouping Content':
+                                $('#groupings').show();
+                                $('#colors, #border_style, #sections, #embeddings, #tables, #forms, #hyperlinks').hide();
+                                break;
+			case 'Embedded Content':
+                                $('#embeddings').show();
+                                $('#colors, #border_style, #sections, #groupings, #tables, #forms, #hyperlinks').hide();
+                                break;
+			case 'Tabular Data':
+                                $('#tables').show();
+                                $('#colors, #border_style, #sections, #embeddings, #groupings, #forms, #hyperlinks').hide();
+                                break;
+			case 'Forms':
+                                $('#forms').show();
+                                $('#colors, #border_style, #sections, #embeddings, #tables, #groupings, #hyperlinks').hide();
+                                break;
+			case 'Hyperlinks':
+                                $('#hyperlinks').show();
+                                $('#colors, #border_style, #sections, #embeddings, #tables, #forms, #groupings').hide();
+                                break;
+			default:
+                                $('#groupings, #sections, #embeddings, #tables, #forms, #hyperlinks').hide();
+			}
+                } /*else if( $(this).text() == $('#html_tags').text() ) {
+                        $('#sections').show();
+                        $('#colors, #border_style, #groupings, #embeddings, #tables, #forms, #hyperlinks').hide();
+                }*/
 	}
 }).change();
--- views/v_styles.php
+++ views/v_styles.php
@@ -1,12 +1,94 @@
 <section id='controls'>
+	<select name='html_elements' id='html_tags'>
+		<option value='0' selected>Choose HTML tags</option>
+		<option value='1'>Sections</option>
+		<option value='2'>Grouping Content</option>
+		<option value='3'>Embedded Content</option>
+		<option value='4'>Tabular Data</option>
+		<option value='5'>Forms</option>
+		<option value='6'>Hyperlinks</option>
+	<select>
+	<ul id='sections'>
+		<li>body</li>
+		<li>div</li>
+		<li>section</li>
+		<li>nav</li>
+		<li>article</li>
+		<li>aside</li>
+		<li>header</li>
+		<li>footer</li>
+		<li>h1</li>
+		<li>h2</li>
+		<li>h3</li>
+		<li>h4</li>
+		<li>h5</li>
+		<li>h6</li>
+	</ul>
+
+	<ul id='groupings'>
+		<li>div</li>
+		<li>p</li>
+		<li>hr</li>
+		<li>ol</li>
+		<li>ul</li>
+		<li>li</li>
+		<li>figure</li>
+		<li>figcaption</li>
+        </ul>
+
+	<ul id='embeddings'>
+		<li>img</li>
+		<li>iframe</li>
+		<li>embed</li>
+		<li>video</li>
+		<li>audio</li>
+		<li>canvas</li>
+        </ul>
+
+	<ul id='tables'>
+		<li>table</li>
+		<li>caption</li>
+		<li>coldroup</li>
+		<li>col</li>
+		<li>tbody</li>
+		<li>thead</li>
+		<li>tfoot</li>
+		<li>tr</li>
+		<li>td</li>
+		<li>th</li>
+        </ul>
 	
+	<ul id='forms'>
+		<li>form</li>
+		<li>fieldset</li>
+		<li>label</li>
+		<li>input</li>
+		<li>button</li>
+		<li>select</li>
+		<li>datalist</li>
+		<li>optgroup</li>
+		<li>option</li>
+		<li>textarea</li>
+        </ul>
+
+	<ul id='hyperlinks'>
+                <li>a</li>
+		<li>a:link</li>
+                <li>a:active</li>
+                <li>a:hover</li>
+                <li>a:visited</li>
+        </ul>
+
+	<div id='braces'>{ }</div>
+
 	<select name='colors' id='color_attr'>
 		<option value='0' selected>Choose Color Attributes</option>
 		<option value='1'>color</option>
 		<option value='2'>background-color</option>
 		<option value='3'>border-color</option>
 	</select>
-	<ul id='colors' class='init_hide'>
+
+	<ul id='colors'>
 		<li id='white'></li>
 		<li id='silver'></li>
 		<li id='gray'></li>
@@ -23,6 +105,25 @@
 		<li id='navy'></li>
 		<li id='fuchsia'></li>
 		<li id='purple'></li>
-		<div class='clearfix'></div>
+	</ul>
+
+	<select name='borders' id='border_attr'>
+                <option value='0' selected>Choose Border Attributes</option>
+                <option value='1'>border-style</option>
+                <option value='2'>border-top-style</option>
+		<option value='3'>border-right-style</option>
+                <option value='4'>border-bottom-style</option>
+		<option value='5'>border-left-style</option>
+        </select>
+	<ul id='border_style'>
+		<li id='none'></li>
+		<li id='dotted'></li>
+		<li id='dashed'></li>
+		<li id='solid'></li>
+		<li id='double'></li>
+		<li id='groove'></li>
+		<li id='ridge'></li>
+		<li id='inset'></li>
+		<li id='outset'></li>
 	</ul>
 </section>
